ff0f11cef9a227f1aed92a5791359ee0d8e100fb
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2026-07-16 09:40:37 +0500

    Stage 4 Full CRUD

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Query
 from pydantic import BaseModel, Field
+from typing import Annotated
+from pydantic.json_schema import SkipJsonSchema
 
 class Task(BaseModel):
     id: int
@@ -8,6 +10,7 @@ class Task(BaseModel):
 
 tasks: list[Task] = []
 tasks.extend([Task(id=1, title="Task 1", done= False) , Task(id= 2, title="Task 2", done=True), Task(id=3, title="Task 3", done=False)])
+task_id_count = 3
 
 app = FastAPI()
 
@@ -34,10 +37,10 @@ def get_task(id: int):
     )
 
 class TaskCreate(BaseModel):
-    title: str
+    title: str=Field(min_length=3, max_length=25)
     done: bool | None = False
 
-@app.post("/tasks")
+@app.post("/tasks", status_code=201)
 def create_task(task: TaskCreate):
     newTitle = task.title.strip()
     if(not newTitle):
@@ -45,7 +48,63 @@ def create_task(task: TaskCreate):
             status_code = status.HTTP_400_BAD_REQUEST,
             detail = f"Title Missing!"
         )
-    newTask = Task(id= len(tasks)+1, title= newTitle, done= task.done)
+    global task_id_count
+    task_id_count += 1
+    newTask = Task(id= task_id_count, title= newTitle, done= task.done)
     tasks.append(newTask)
     return newTask
+
+def find_by_id(id: int) -> int:
+    for i in range(0, len(tasks)):
+        if(id == tasks[i].id):
+            return i
+    return -1
+
+class TaskUpdate(BaseModel):
+    title: str=Field(min_length=3, max_length=25)
+    done: bool 
+
+# task_id = task index in the tasks array
+@app.put("/tasks/{id}")
+def update_task(id: int, new_task: TaskUpdate):
+    task_id = find_by_id(id)
+    if(task_id == -1):
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail= f"Task with {id} not found"
+        )
+    tasks[task_id].title = new_task.title
+    tasks[task_id].done = new_task.done
+    return tasks[task_id]
     
+
+@app.patch("/tasks/{id}")
+def patch_task(id: int, title: Annotated[str | SkipJsonSchema[None], Query(min_length=3, max_length=25)] = None, done: Annotated[bool | SkipJsonSchema[None], Query()] = None):
+    task_id = find_by_id(id)
+    if(task_id == -1):
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail= f"Task with {id} not found"
+        )
+    if title is None and done is None:
+        raise HTTPException(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            detail= "Missing title and status!"
+        )
+    if title is not None:
+        title = title.strip()
+        if title:
+            tasks[task_id].title = title
+    if done is not None:
+        tasks[task_id].done = done
+    return tasks[task_id]
+
+@app.delete("/tasks/{id}", status_code=204)
+def delete_task(id: int):
+    task_id = find_by_id(id)
+    if(task_id == -1):
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail= f"Task with {id} not found"
+        ) 
+    tasks.pop(task_id)
