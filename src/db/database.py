afde53341ef39a3017747d86f4d7eabfef2a865f
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2026-07-31 03:25:32 +0500

    A3: Stage 1 -> connect via .env and create table

--- src/db/database.py
+++ src/db/database.py
@@ -10,8 +10,7 @@ load_dotenv()
 
 db_url = os.getenv("DB_URL")
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(db_url, echo=True, connect_args=connect_args)
+engine = create_engine(db_url, echo=True)
 
 
 def create_db_and_tables():
